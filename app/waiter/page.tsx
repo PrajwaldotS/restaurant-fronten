cf129d6bb28f361d753016debbd0abddfe5c9375
prajwaldots <prajwal.s2294@gmail.com>
2026-02-26 20:59:51 +0530

    minor changes made

--- app/waiter/page.tsx
+++ app/waiter/page.tsx
@@ -118,7 +118,7 @@ const markPicked = async (order: any) => {
         number: total,
         method: "cash",
         payment_status: "pending",
-        order: order.id   // ✅ CORRECT RELATION FIELD
+        order: order.id  
       }
     })
   })
